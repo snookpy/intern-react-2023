727bdd78c0c9341b6a35561ddd7f79df7a4d0bf9
Nook <nookittipon@gmail.com>
2023-05-02 02:55:13 +0700

    premiter

--- src/components/MatApp/InitRoute.tsx
+++ src/components/MatApp/InitRoute.tsx
@@ -1,28 +1,28 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom"
-import ExampleDnDPage from "../ExampleDnDPage/ExampleDnDPage"
-import ExampleFormPage from "../ExampleFormPage/ExampleFormPage"
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ExampleDnDPage from '../ExampleDnDPage/ExampleDnDPage'
+import ExampleFormPage from '../ExampleFormPage/ExampleFormPage'
 
-import MatLayout from "./MatLayout"
+import MatLayout from './MatLayout'
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <MatLayout />,
-		children: [
-			{
-				path: "/",
-				element: <ExampleDnDPage />,
-			},
-			{
-				path: "form",
-				element: <ExampleFormPage />,
-			},
-		],
-	},
+    {
+        path: '/',
+        element: <MatLayout />,
+        children: [
+            {
+                path: '/',
+                element: <ExampleDnDPage />    ,
+            },
+            {
+                path: 'form',
+                element: <ExampleFormPage />,
+            },
+        ],
+    },
 ])
 
 function InitRoute() {
-	return <RouterProvider router={router} />
+    return <RouterProvider router={router} />
 }
 
 export default InitRoute
