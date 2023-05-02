d7e2d8c8edf5e4d76abe97cde7658dfbe7c210e5
Tulakorn Sawangmuang <sunnytulakorn@gmail.com>
2023-05-02 16:12:21 +0700

    PrimeNumberHW

--- src/hooks/usePrimeNumber/usePrimeNumber.ts
+++ src/hooks/usePrimeNumber/usePrimeNumber.ts
@@ -1,10 +1,28 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 const usePrimeNumber = () => {
-	const calIsPrime = (num: number) => {
-	}
+    const [_num, setNum] = useState<number>(0)
+    const [isPrime, setIsPrime] = useState<boolean>(false)
 
-	return [true, calIsPrime, 0] as const
+    const calIsPrime = (num: number) => {
+        setNum(num)
+
+        if (num <= 0) {
+            setIsPrime(false)
+            return
+        }
+
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                setIsPrime(false)
+                return
+            }
+        }
+
+        setIsPrime(true)
+    }
+
+    return [isPrime, calIsPrime, _num] as const
 }
 
 export default usePrimeNumber
